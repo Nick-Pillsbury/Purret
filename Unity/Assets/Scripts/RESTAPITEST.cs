26d899ea7b44cb9e0a09985f5427572c92730519
Tarik Baghdadi <tarikkbaghdadi@gmail.com>
2026-01-27 17:11:54 -0500

    Initial rest api testing

--- Unity/Assets/Scripts/RESTAPITEST.cs
+++ Unity/Assets/Scripts/RESTAPITEST.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
 
 public class RESTAPITEST : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        UnityWebRequest request = UnityWebRequest.Get("https://jsonplaceholder.typicode.com/todos/1");
+        StartCoroutine(SendRequest(request));
         
     }
 
@@ -12,5 +16,22 @@ public class RESTAPITEST : MonoBehaviour
     void Update()
     {
         
+    }
+
+    IEnumerator SendRequest(UnityWebRequest request)
+    {
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+        else
+        {
+            string jsonResponse = request.downloadHandler.text;
+
+
+            Debug.Log("Response: " + jsonResponse);
+        }
     }
 }
