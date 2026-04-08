4da43e2ae7be570988634f2af54f1d7e757c4f7a
Tarik Baghdadi <tarikkbaghdadi@gmail.com>
2026-04-07 20:01:44 -0400

    got camera movement functional

--- Unity/Assets/Scripts/CameraMovementScript.cs
+++ Unity/Assets/Scripts/CameraMovementScript.cs
@@ -1,46 +1,54 @@
+using System;
 using System.Collections;
-using TMPro;
-using Unity.VisualScripting;
+using System.Text;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.Networking;
-using UnityEngine.SceneManagement;
+
+[Serializable]
+public class ServoMoveRequestBody
+{
+    public float x;
+    public float y;
+    public int step;
+}
 
 public class CameraMovementScript : MonoBehaviour
 {
     public float currentSpeed = 50f;
     private Vector2 moveDir;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool isSending;
+
+    private void Start()
     {
+        StartCoroutine(ResetServo());
         
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        moveDir = InputSystem.actions[ "Move" ].ReadValue<Vector2>();
-        if (moveDir != Vector2.zero)
+        moveDir = InputSystem.actions["Move"].ReadValue<Vector2>();
+
+        if (moveDir != Vector2.zero && !isSending)
         {
             Debug.Log("Moving in direction: " + moveDir);
-            StartCoroutine(MoveServo(moveDir * (currentSpeed/100f))); // Scale movement by current speed
-            // Implement movement logic here, e.g., send movement commands to the robot
+            StartCoroutine(MoveServo(moveDir));
         }
     }
 
-    IEnumerator MoveServo(Vector2 direction)
+    IEnumerator ResetServo()
     {
-
-        var request = new UnityWebRequest("http://10.0.0.1:8000/front/servo/move", "POST");
-        request.downloadHandler = new DownloadHandlerBuffer();
+        isSending = true;
+        var request = new UnityWebRequest("http://10.0.0.1:8000/front/servo/reset", "POST");
         request.uploadHandler = new UploadHandlerRaw(new byte[0]);
+        request.downloadHandler = new DownloadHandlerBuffer();
+
         request.SetRequestHeader("Authorization", "Bearer " + RoleManagerScript.token);
+        request.SetRequestHeader("Content-Type", "application/json");
         request.SetRequestHeader("Accept", "application/json");
-        
+
         yield return request.SendWebRequest();
-        
+
         if (request.result != UnityWebRequest.Result.Success)
         {
             Debug.Log(request.responseCode + " " + request.error);
@@ -51,11 +59,50 @@ public class CameraMovementScript : MonoBehaviour
             Debug.Log("Servo moved!");
             Debug.Log(request.downloadHandler.text);
         }
+
+        isSending = false;
+    }
+
+    IEnumerator MoveServo(Vector2 direction)
+    {
+        isSending = true;
+
+        ServoMoveRequestBody body = new ServoMoveRequestBody
+        {
+            x = Mathf.Clamp(direction.x, -1f, 1f),
+            y = Mathf.Clamp(direction.y, -1f, 1f),
+            step = Mathf.Clamp(Mathf.RoundToInt(currentSpeed / 10f), 1, 30)
+        };
+
+        string json = JsonUtility.ToJson(body);
+        byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
+
+        var request = new UnityWebRequest("http://10.0.0.1:8000/front/servo/move", "POST");
+        request.uploadHandler = new UploadHandlerRaw(jsonBytes);
+        request.downloadHandler = new DownloadHandlerBuffer();
+
+        request.SetRequestHeader("Authorization", "Bearer " + RoleManagerScript.token);
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Accept", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.responseCode + " " + request.error);
+            Debug.Log(request.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log("Servo moved!");
+            Debug.Log(request.downloadHandler.text);
+        }
+
+        isSending = false;
     }
 
     public void AdjustSpeed(float newSpeed)
     {
-        // Implement speed adjustment functionality here
-        currentSpeed = Mathf.Clamp(newSpeed, 0f, 100f); // Assuming speed range is 0 to 100
+        currentSpeed = Mathf.Clamp(newSpeed, 0f, 100f);
     }
 }
